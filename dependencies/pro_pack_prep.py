88f6d56fdaae3dbe242a15eca9e0274b3bbe5c08
rjkiv <76180273+rjkiv@users.noreply.github.com>
2023-02-17 12:41:29 -0700

    action build for pro strings (#14)
    * add workflow
    
    * use wget over curl
    
    * test run onyx
    
    * change permissions of onyx executable
    
    * no permissions, just executable
    
    * sudo
    
    * change directory of onyx
    
    * add libfuse dependency
    
    * add path
    
    * extract app package
    
    * onyx stfs help output
    
    * create pro pack py script
    
    * fix workflow syntax
    
    * create CON file contents
    
    * upload con
    
    * make output dir
    
    * update readmes
    
    ---------
    
    Co-authored-by: rjkiv <rjkiv@users.noreply.github.com>

---
+++ dependencies/pro_pack_prep.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+# get the current working directory
+cwd = Path().absolute()
+print(cwd)
+
+mega_upgrade_dta = []
+cwd.joinpath("_tmp/songs_upgrades").mkdir(parents=True, exist_ok=True)
+
+# traverse through rb3_plus/Pro Strings and find both _plus.mid and upgrades.dta
+for pro_song in cwd.glob("Pro Strings/*/*"):
+    print(pro_song.stem)
+    for pro_file in pro_song.glob("*"):
+        # if working with a dta, append it to the mega dta in here
+        if pro_file.name == "upgrades.dta":
+            mega_upgrade_dta.extend([line for line in open(pro_file, "r")])
+            mega_upgrade_dta.append("\n")
+        # if working with a mid, copy it
+        elif "_plus.mid" in pro_file.name:
+            # copy the mid from rb3_plus directly into the song update folder
+            destination_path = cwd.joinpath(f"_tmp/songs_upgrades/{pro_file.name}")
+            destination_path.write_bytes(pro_file.read_bytes())
+
+with open(cwd.joinpath("_tmp/songs_upgrades/upgrades.dta"),"w",encoding="ISO-8859-1") as dta_output:
+        dta_output.writelines(mega_upgrade_dta)
+        
